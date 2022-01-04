07001935244bd83aa4caeef398f4dc505ae94330
Angel <47459464+vihangel@users.noreply.github.com>
2022-01-04 10:25:38 -0300

    rodou.

---
+++ src/app/Controllers/UserController.js
@@ -0,0 +1,15 @@
+class UserController {
+  index(req, res) {
+    console.log(req.body);
+  }
+
+  show(req, res) {
+    var users = ["Kaio", "Larissa", "Danver"];
+    return res.status(200).json({
+      error: false,
+      users,
+    });
+  }
+}
+
+module.exports = new UserController();
