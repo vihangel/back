31bbab65e61204cc15fdfa1b2c39e7f4f80cd00b
Angel <47459464+vihangel@users.noreply.github.com>
2022-01-05 12:30:06 -0300

    Update connection.js

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,14 +1,15 @@
 const mongoose = require("mongoose");
 
 const URI = process.env.MONGO_URL;
-URI =
-  "mongodb+srv://angel:psicotran@psicotran.rctxh.mongodb.net/psicotran?retryWrites=true&w=majority";
+
 class Connection {
   constructor() {
     this.dataBaseConnectionMongoDB();
   }
 
   dataBaseConnectionMongoDB() {
+    URI =
+      "mongodb+srv://angel:psicotran@psicotran.rctxh.mongodb.net/psicotran?retryWrites=true&w=majority";
     this.mongoDBConnection = mongoose
       .connect(URI, {
         useNewUrlParser: true,
