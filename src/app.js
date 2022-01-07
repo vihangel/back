5f213f14ec96842e7c12454f5c5af692d2b094c7
Angel <47459464+vihangel@users.noreply.github.com>
2022-01-07 15:27:56 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,6 +15,7 @@ class App {
     this.app.use(express.json());
 
     this.app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Origin: *");
       res.header(
         "Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE"
@@ -22,13 +23,12 @@ class App {
       res.header(
         "Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With"
       );
-      req.header("Access-Control-Allow-Origin: *");
-      req.header(
-        "Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE"
+
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Access, Content-type, Authorization, Accept, Origin, X-Requested-With"
       );
-      req.header(
-        "Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With"
-      );
+
       this.app.use(cors());
       next();
     });
