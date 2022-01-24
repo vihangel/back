deda3a160c27a1eb946b071bd6727ef51307c834
Angel <47459464+vihangel@users.noreply.github.com>
2022-01-24 18:44:20 -0300

    ahh

--- src/app/Controllers/UserController.js
+++ src/app/Controllers/UserController.js
@@ -72,53 +72,6 @@ class UserController {
       });
     });
   }
-
-  //Alterar usuario depois
-  async set(req, res) {
-    //validação
-    let schema = yup.object().shape({
-      cpf: yup.string().required(),
-      renach: yup.string().required(),
-    });
-    console.log(req.body);
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({
-        error: true,
-        message: "Dados inválidos",
-      });
-    }
-
-    let userExist = await User.findOne({ cpf: req.body.cpf });
-    if (!userExist) {
-      return res.status(400).json({
-        error: true,
-        message: "Este usuário não existe",
-      });
-    }
-
-    const { user } = req.body;
-
-    if (fullUserr == null) {
-      //Ver qual o codigo de bad request
-      return res.status(400).json({
-        error: true,
-        message: "Algo deu errado, tente novamente!",
-      });
-    }
-
-    let user = await User.create(fullUser, (err) => {
-      if (err)
-        return res.status(400).json({
-          error: true,
-          message: "Erro ao tentar inserir usuário no MongoDB",
-        });
-
-      return res.status(200).json({
-        error: false,
-        message: "Usuário cadastrado com sucesso",
-      });
-    });
-  }
 }
 
 module.exports = new UserController();
